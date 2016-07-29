2218843a12d925474adb657f20dd781954651b99
Gonen Dukas <gonenduk@gmail.com>
2016-07-29 21:16:12 +0300

    Cluster support

--- bin/instance.js
+++ bin/instance.js
@@ -1,8 +1,9 @@
-module.exports = config => {
+module.exports = (config, worker) => {
 
-  // Store configuration in express
+  // Store configuration and worker in express
   const express = require('express');
   express.config = config;
+  express.worker = worker;
 
   // Load app and http server
   const app = require('../app');
@@ -46,15 +47,16 @@ module.exports = config => {
     const bind = typeof port === 'string'
       ? 'Pipe ' + port
       : 'Port ' + port;
+    const id = worker ? `(${worker.id}) ` : '';
 
     // handle specific listen errors with friendly messages
     switch (error.code) {
       case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
+        console.error(`${id}${bind} requires elevated privileges`);
         process.exit(1);
         break;
       case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
+        console.error(`${id}${bind} is already in use`);
         process.exit(1);
         break;
       default:
@@ -68,6 +70,7 @@ module.exports = config => {
     const bind = typeof addr === 'string'
       ? 'pipe ' + addr
       : 'port ' + addr.port;
-    console.log('Listening on ' + bind);
+    const id = worker ? `(${worker.id}) ` : '';
+    console.log(`${id}Listening on ${bind}`);
   }
 };
--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
 // Use strict mode in all project modules
+'use strict';
 require('use-strict');
 
-// Load configuration
+// Load modules
 const config = require('config');
+const cluster = require('express-cluster');
 
-// Initialize server instance with configuration
-require('./instance')(config);
+// Initialize server instances with configuration
+if (config.cluster) {
+  cluster(worker => {
+    require('./instance')(config, worker);
+  }, config.cluster);
+
+// Initialize single server in master process
+} else {
+  require('./instance')(config);
+}

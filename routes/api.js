db54020e3b9c000443a95f4bcd60f616f662cd64
Gonen Dukas <gonenduk@gmail.com>
2016-09-03 15:17:59 +0300

    routes should be from the inner out

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,11 @@ const controllers = require('../controllers');
 const Celebrate = require('celebrate');
 const router = express.Router();
 
-// API root
-router.get('/', controllers.api.get);
-
 // User
 router.get('/users/:id', Celebrate(validations.user.get), controllers.user.get);
+
+// API root
+router.get('/', controllers.api.get);
 
 // Error handlers
 router.use(controllers.api.error404);

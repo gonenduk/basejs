64af483a7bf44481a1d7ff6f73b6dc3a5db1d0a8
Gonen Dukas <gonenduk@gmail.com>
2016-09-05 21:14:01 +0300

    email support using promises and lazy load of module

---
+++ modules/email/email.js
@@ -0,0 +1,25 @@
+const app = require('../../app');
+const nodemailer = require('nodemailer');
+
+// Create transporter
+const transporter = nodemailer.createTransport({ service: config.email.service }, config.email);
+
+// Promisify
+const render = Promise.promisify(app.render, { context: app });
+const sendMail = Promise.promisify(transporter.sendMail, { context: transporter });
+
+// Send email
+function send(options, data) {
+  // Default template
+  options.template = options.template || config.email.template;
+
+  // Render template into html
+  return render('emails/' + options.template, data).then((html) => {
+    options.html = html;
+
+    // Send email
+    return sendMail(options);
+  });
+}
+
+module.exports = { send };

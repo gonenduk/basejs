07410256f671ba2f379a8f768e98962aaf36617c
Gonen Dukas <gonenduk@gmail.com>
2016-09-19 11:10:42 +0300

    New favicon

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // General app setup
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(morgan(config.morgan.format, { stream: logger.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

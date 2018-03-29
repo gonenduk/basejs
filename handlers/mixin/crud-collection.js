d5257560a623a24ce07e4b423935e7342647c077
Gonen Dukas <gonenduk@gmail.com>
2018-03-30 01:12:02 +0300

    mixin in handlers for common cases

---
+++ handlers/mixin/crud-collection.js
@@ -0,0 +1,32 @@
+const Boom = require('boom');
+
+module.exports = function (model, handler = {}) {
+  const mixin = Object.create(handler);
+
+  mixin.get = async (req, res, next) => {
+    // Get filter and sort objects (in OpenAPI 3 won't need to verify they are objects)
+    let filter, sort, operation;
+    try {
+      operation = 'filter';
+      if (req.query.filter) filter = JSON.parse(req.query.filter);
+      operation = 'sort';
+      if (req.query.sort) sort = JSON.parse(req.query.sort);
+    } catch (err) {
+      return next(Boom.badRequest(`Invalid ${operation}: ${err.message}`));
+    }
+
+    // Get offset and limit
+    const offset = req.query.offset;
+    const limit = req.query.limit;
+
+    // Get list of items
+    try {
+      res.json(await model.getAll(filter, sort, offset, limit));
+    }
+    catch (err) {
+      next(err);
+    }
+  };
+
+  return mixin;
+};
--- handlers/users.js
+++ handlers/users.js
@@ -1,29 +1,4 @@
 const User = require('../models/user');
-const Boom = require('boom');
+const crudCollectionMixin = require('./mixin/crud-collection');
 
-module.exports = {
-  get: async (req, res, next) => {
-    // Get filter and sort objects (in OpenAPI 3 won't need to verify they are objects)
-    let filter, sort, operation;
-    try {
-      operation = 'filter';
-      if (req.query.filter) filter = JSON.parse(req.query.filter);
-      operation = 'sort';
-      if (req.query.sort) sort = JSON.parse(req.query.sort);
-    } catch (err) {
-      return next(Boom.badRequest(`Invalid ${operation}: ${err.message}`));
-    }
-
-    // Get offset and limit
-    const offset = req.query.offset;
-    const limit = req.query.limit;
-
-    // Get list of users
-    try {
-      res.json(await User.getAll(filter, sort, offset, limit));
-    }
-    catch (err) {
-      next(err);
-    }
-  }
-};
+module.exports = crudCollectionMixin(User);

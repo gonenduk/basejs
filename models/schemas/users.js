e3a0213a1ca3cee6b12e11fd2ec632159f64fc66
Gonen Dukas <gonenduk@gmail.com>
2018-04-07 11:07:05 +0300

    user roles in swagger and db schema

--- models/schemas/users.js
+++ models/schemas/users.js
@@ -1,11 +1,11 @@
 module.exports = {
   $jsonSchema: {
     bsonType: "object",
-    required: ["username"],
+    required: ["username", "role"],
     properties: {
       username: { bsonType: "string" },
       password: { bsonType: "string" },
-      role: {bsonType: "string"},
+      role: { enum: ["god", "admin", "moderator", "user", "guest"] },
       createdAt: { bsonType: "date" },
       updatedAt: { bsonType: "date" }
     }

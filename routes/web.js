2b986715db5ef23821b241331b6e8ce4c4d04f6e
Gonen Dukas <gonenduk@gmail.com>
2018-04-09 10:37:04 +0300

    allow server side google analytics on web pages

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const config = require('config');
+const ua = require('../lib/analytics');
 const handlers = require('../handlers');
 const Boom = require('boom');
 const router = express.Router();
+
+// Google analytics (server side)
+if (config.analytics && config.analytics.web) {
+  router.use('/', (req, res, next) => {
+    const visitor = ua(req.user.id);
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (ip !== '::1') visitor.set('uip', ip);
+    visitor.pageview(req.originalUrl).send();
+    next();
+  });
+}
 
 // Pages
 router.get('/', handlers.web.home);

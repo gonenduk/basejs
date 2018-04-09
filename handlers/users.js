9b53d1f660ce4159bfaa2d4497e14f7d5b231e80
Gonen Dukas <gonenduk@gmail.com>
2018-04-09 21:21:25 +0300

    block non super users from getting all users

--- handlers/users.js
+++ handlers/users.js
@@ -1,15 +1,25 @@
 const user = require('../models/user');
+const roles = require('../lib/roles');
 const ResourceCollection = require('./plugins/resource-collection');
+const Boom = require('boom');
 
 class UsersHandler extends ResourceCollection {
   constructor() {
     super(user);
   }
 
-  // Hide password (write only)
-  async get(req, res, next) {
-    req.query.projection = { password: false };
-    return super.get(req, res, next);
+  // Block non super users and hide password (write only)
+  get(req, res, next) {
+    if (roles.isSuperUser(req.user.role)) {
+      req.query.projection = { password: false };
+      return super.get(req, res, next);
+    } else
+      return next(Boom.forbidden('Access denied'));
+  }
+
+  // Block non super users
+  post(req, res, next) {
+    return super.post(req, res, next);
   }
 }
 

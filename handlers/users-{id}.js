73850a49e400e1f7f3d87a36eb951c5eb08d9643
Gonen Dukas <gonenduk@gmail.com>
2018-04-09 21:56:47 +0300

    Block normal users from viewing other users

--- handlers/users-{id}.js
+++ handlers/users-{id}.js
@@ -1,15 +1,22 @@
 const user = require('../models/user');
+const roles = require('../lib/roles');
 const ResourceItem = require('./plugins/resource-item');
+const Boom = require('boom');
 
 class UserHandler extends ResourceItem {
   constructor() {
     super(user);
   }
 
-  // Hide password (write only)
+  // Block normal users from viewing other users and hide password (write only)
   async get(req, res, next) {
-    req.query.projection = { password: false };
-    return super.get(req, res, next);
+    if (req.pathParams.id === 'me') req.pathParams.id = req.user.id;
+
+    if (req.pathParams.id === req.user.id || roles.isSuperUser(req.user.role)) {
+      req.query.projection = { password: false };
+      return super.get(req, res, next);
+    } else
+      return next(Boom.forbidden('Access denied'));
   }
 }
 

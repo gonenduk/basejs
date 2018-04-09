49078bcf8fecccf052cf24a85144d9106f984df4
Gonen Dukas <gonenduk@gmail.com>
2018-04-09 16:01:04 +0300

    add ownerId to product schema and api

--- models/schemas/products.js
+++ models/schemas/products.js
@@ -2,10 +2,12 @@ module.exports = {
   schema: {
     $jsonSchema: {
       bsonType: "object",
-      required: ["title", "price"],
+      required: ["title", "ownerId"],
       properties: {
         title: { bsonType: "string" },
         price: { bsonType: "number" },
+        // Shared properties
+        ownerId: { bsonType: "objectId" },
         createdAt: { bsonType: "date" },
         updatedAt: { bsonType: "date" }
       }

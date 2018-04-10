3c925a3b0de24c414053b618758e8997d09c8da5
Gonen Dukas <gonenduk@gmail.com>
2018-04-10 16:23:07 +0300

    small remark fix

--- handlers/plugins/collection-ownership.js
+++ handlers/plugins/collection-ownership.js
@@ -3,7 +3,7 @@ const Boom = require('boom');
 module.exports = ResourceClass => {
   return class extends ResourceClass {
     post(req, res, next) {
-      // Set ownership of current user
+      // Set item owner to current user
       req.body.ownerId = req.user.id;
       return super.post(req, res, next);
     }

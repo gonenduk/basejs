e0d379394f57df3b0d12cdbe7760fe70c1e7f905
Gonen Dukas <gonenduk@gmail.com>
2018-04-10 16:18:09 +0300

    fix ownership id in model

--- models/plugins/ownership.js
+++ models/plugins/ownership.js
@@ -3,8 +3,7 @@ const ObjectId = require('mongodb').ObjectId;
 module.exports = ModelClass => {
   return class extends ModelClass {
     addOne(item = {}) {
-      // Default owner is current user
-      if (!item.ownerId) item.ownerId = new ObjectId(item.ownerId);
+      item.ownerId = new ObjectId(item.ownerId);
       return super.addOne(item);
     }
   };

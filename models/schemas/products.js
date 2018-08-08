2dff96a4d4b4d52eb6f828bb4223b45194962673
Gonen Dukas <gonenduk@gmail.com>
2018-08-09 01:20:37 +0300

    models schema in airbnb style code

--- models/schemas/products.js
+++ models/schemas/products.js
@@ -1,19 +1,19 @@
 module.exports = {
   schema: {
     $jsonSchema: {
-      bsonType: "object",
-      required: ["title", "ownerId"],
+      bsonType: 'object',
+      required: ['title', 'ownerId'],
       properties: {
-        title: { bsonType: "string" },
-        price: { bsonType: "number" },
+        title: { bsonType: 'string' },
+        price: { bsonType: 'number' },
         // Shared properties
-        ownerId: { bsonType: "objectId" },
-        createdAt: { bsonType: "date" },
-        updatedAt: { bsonType: "date" }
-      }
-    }
+        ownerId: { bsonType: 'objectId' },
+        createdAt: { bsonType: 'date' },
+        updatedAt: { bsonType: 'date' },
+      },
+    },
   },
   indexes: [
-    { fields: { "ownerId": 1 }, options: {} }
-  ]
+    { fields: { ownerId: 1 }, options: {} },
+  ],
 };
--- models/schemas/users.js
+++ models/schemas/users.js
@@ -1,19 +1,19 @@
 module.exports = {
   schema: {
     $jsonSchema: {
-      bsonType: "object",
-      required: ["email", "role"],
+      bsonType: 'object',
+      required: ['email', 'role'],
       properties: {
-        email: { bsonType: "string" },
-        password: { bsonType: "string" },
-        role: { enum: ["user", "moderator", "admin"] },
-        createdAt: { bsonType: "date" },
-        updatedAt: { bsonType: "date" },
-        logoutAt: { bsonType: "date" }
-      }
-    }
+        email: { bsonType: 'string' },
+        password: { bsonType: 'string' },
+        role: { enum: ['user', 'moderator', 'admin'] },
+        createdAt: { bsonType: 'date' },
+        updatedAt: { bsonType: 'date' },
+        logoutAt: { bsonType: 'date' },
+      },
+    },
   },
   indexes: [
-    { fields: { "email": 1 }, options: { unique: true } }
-  ]
+    { fields: { email: 1 }, options: { unique: true } },
+  ],
 };

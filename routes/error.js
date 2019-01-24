1bc826cb1bfa248eeccb3faf3d21c136cd772ff0
Gonen Dukas <gonenduk@gmail.com>
2019-01-24 02:08:10 +0200

    Boom errors get status 500 by default

--- routes/error.js
+++ routes/error.js
@@ -4,7 +4,7 @@ const logger = require('../lib/logger');
 
 module.exports = (err, req, res, next) => {
   // Convert error to Boom error and set status to 500 if not set
-  const errPayload = Boom.boomify(err, { statusCode: err.status || 500, override: false }).output.payload;
+  const errPayload = Boom.boomify(err, { statusCode: err.status, override: false }).output.payload;
 
   // Log stack on server errors
   if (errPayload.statusCode === 500) logger.error(err.stack);

c34263fef1da2f1d509893b411944816ba6fd671
Gonen Dukas <gonenduk@gmail.com>
2019-10-28 02:19:22 +0200

    more tests for acl

--- acl/resources/base.js
+++ acl/resources/base.js
@@ -33,9 +33,9 @@ module.exports = {
 
     owner: {
       put: (req, res, next) => {
-        let permission = ac.can(req.user.role).updateAny('owner');
+        let permission = ac.can(req.user.role).updateAny('resource-owner');
         if (!permission.granted) {
-          permission = ac.can(req.user.role).updateOwn('owner');
+          permission = ac.can(req.user.role).updateOwn('resource-owner');
           if (!permission.granted) throw Boom.forbidden();
           req.query.filter = { ownerId: req.user.id };
         }
---
+++ test/acl/private-resources.spec.js
@@ -0,0 +1,46 @@
+const assert = require('assert').strict;
+const Boom = require('@hapi/boom');
+require('../../acl');
+const privateResourceACL = require('../../acl/resources/private');
+
+const moderator = { role: 'moderator', id: '2' };
+const user = { role: 'user', id: '3' };
+
+const req = {};
+const res = {};
+const next = () => {};
+
+describe('Access control for private resources', () => {
+  beforeEach(() => {
+    req.query = {};
+  });
+
+  context('Get list', () => {
+    it('should allow moderator to read', () => {
+      req.user = moderator;
+      assert.doesNotThrow(() => { privateResourceACL.get(req, res, next); });
+    });
+
+    it('should not allow user to read', () => {
+      req.user = user;
+      assert.throws(() => { privateResourceACL.get(req, res, next); }, Boom.forbidden());
+    });
+  });
+
+  context('Get resource', () => {
+    it('should allow user to read own', () => {
+      req.user = user;
+      assert.doesNotThrow(() => { privateResourceACL[':id'].get(req, res, next); });
+    });
+
+    it('should not allow user to read any', () => {
+      req.user = user;
+      assert.throws(() => { privateResourceACL[':id'].get(req, res, next); }, Boom.forbidden());
+    });
+
+    it('should allow moderator to read any', () => {
+      req.user = moderator;
+      assert.doesNotThrow(() => { privateResourceACL[':id'].get(req, res, next); });
+    });
+  });
+});

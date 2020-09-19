33420ca700718e7235c610338543d732a0d69d06
Gonen Dukas <gonenduk@gmail.com>
2020-09-20 00:53:33 +0300

    full coverage unit tests for public and private resources acl

--- test/acl/private-resources.spec.js
+++ test/acl/private-resources.spec.js
@@ -4,8 +4,10 @@ require('../../acl');
 const router = require('../../acl/validations/private');
 const errorRouter = require('../../routes/error');
 
+const admin = { role: 'admin', id: '1' };
 const moderator = { role: 'moderator', id: '2' };
 const user = { role: 'user', id: '3' };
+const guest = { role: 'guest' };
 let testUser;
 
 const app = express();
@@ -38,6 +40,22 @@ describe('Access control for private resources', () => {
     });
   });
 
+  context('Create resource', () => {
+    it('should allow user to create', (done) => {
+      testUser = user;
+      request(app)
+        .post('/')
+        .expect(200, done);
+    });
+
+    it('should not allow guest to create', (done) => {
+      testUser = guest;
+      request(app)
+        .post('/')
+        .expect(403, done);
+    });
+  });
+
   context('Get resource', () => {
     it('should allow user to read own', (done) => {
       testUser = user;
@@ -58,6 +76,68 @@ describe('Access control for private resources', () => {
       request(app)
         .get('/4')
         .expect(200, { ownerId: '3' }, done);
+    });
+  });
+
+  context('Update resource', () => {
+    it('should allow user to update own', (done) => {
+      testUser = user;
+      request(app)
+        .patch('/3')
+        .expect(200, { ownerId: '3' }, done);
+    });
+
+    it('should not allow moderator to update any', (done) => {
+      testUser = moderator;
+      request(app)
+        .patch('/3')
+        .expect(200, { ownerId: '2' }, done);
+    });
+
+    it('should allow admin to update any', (done) => {
+      testUser = admin;
+      request(app)
+        .patch('/3')
+        .expect(200, {}, done);
+    });
+  });
+
+  context('Delete resource', () => {
+    it('should allow user to delete own', (done) => {
+      testUser = user;
+      request(app)
+        .delete('/3')
+        .expect(200, { ownerId: '3' }, done);
+    });
+
+    it('should not allow moderator to delete any', (done) => {
+      testUser = moderator;
+      request(app)
+        .delete('/3')
+        .expect(200, { ownerId: '2' }, done);
+    });
+
+    it('should allow admin to delete any', (done) => {
+      testUser = admin;
+      request(app)
+        .delete('/3')
+        .expect(200, {}, done);
+    });
+  });
+
+  context('Update resource ownership', () => {
+    it('should not allow moderator to update own', (done) => {
+      testUser = moderator;
+      request(app)
+        .put('/2/owner')
+        .expect(403, done);
+    });
+
+    it('should allow admin to update any', (done) => {
+      testUser = admin;
+      request(app)
+        .put('/2/owner')
+        .expect(200, {}, done);
     });
   });
 });
--- test/acl/public-resources.spec.js
+++ test/acl/public-resources.spec.js
@@ -4,6 +4,9 @@ require('../../acl');
 const router = require('../../acl/validations/public');
 const errorRouter = require('../../routes/error');
 
+const admin = { role: 'admin', id: '1' };
+const moderator = { role: 'moderator', id: '2' };
+const user = { role: 'user', id: '3' };
 const guest = { role: 'guest' };
 let testUser;
 
@@ -30,11 +33,89 @@ describe('Access control for public resources', () => {
     });
   });
 
+  context('Create resource', () => {
+    it('should allow user to create', (done) => {
+      testUser = user;
+      request(app)
+        .post('/')
+        .expect(200, done);
+    });
+
+    it('should not allow guest to create', (done) => {
+      testUser = guest;
+      request(app)
+        .post('/')
+        .expect(403, done);
+    });
+  });
+
   context('Get resource', () => {
     it('should allow guest to read any', (done) => {
       testUser = guest;
       request(app)
         .get('/3')
+        .expect(200, {}, done);
+    });
+  });
+
+  context('Update resource', () => {
+    it('should allow user to update own', (done) => {
+      testUser = user;
+      request(app)
+        .patch('/3')
+        .expect(200, { ownerId: '3' }, done);
+    });
+
+    it('should not allow moderator to update any', (done) => {
+      testUser = moderator;
+      request(app)
+        .patch('/3')
+        .expect(200, { ownerId: '2' }, done);
+    });
+
+    it('should allow admin to update any', (done) => {
+      testUser = admin;
+      request(app)
+        .patch('/3')
+        .expect(200, {}, done);
+    });
+  });
+
+  context('Delete resource', () => {
+    it('should allow user to delete own', (done) => {
+      testUser = user;
+      request(app)
+        .delete('/3')
+        .expect(200, { ownerId: '3' }, done);
+    });
+
+    it('should not allow moderator to delete any', (done) => {
+      testUser = moderator;
+      request(app)
+        .delete('/3')
+        .expect(200, { ownerId: '2' }, done);
+    });
+
+    it('should allow admin to delete any', (done) => {
+      testUser = admin;
+      request(app)
+        .delete('/3')
+        .expect(200, {}, done);
+    });
+  });
+
+  context('Update resource ownership', () => {
+    it('should not allow moderator to update own', (done) => {
+      testUser = moderator;
+      request(app)
+        .put('/2/owner')
+        .expect(403, done);
+    });
+
+    it('should allow admin to update any', (done) => {
+      testUser = admin;
+      request(app)
+        .put('/2/owner')
         .expect(200, {}, done);
     });
   });

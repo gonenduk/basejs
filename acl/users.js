57ac76059d993bc514408415e9b172e08ba4a7c8
gonenduk <gonenduk@gmail.com>
2022-02-27 14:36:59 +0200

    users acl not as routes

--- acl/users.js
+++ acl/users.js
@@ -1,30 +1,8 @@
-const express = require('express');
 const validate = require('./base-validation');
 
-const router = express.Router();
-
-router.get('/users', (req, res, next) => {
-  validate.ownOrAnyByUserId(req.user, 'read', 'resource');
-  next();
-});
-
-router.post('/users', (req, res, next) => {
-  next();
-});
-
-router.get('/users/:id', (req, res, next) => {
-  validate.ownOrAnyByUserId(req.user, 'read', 'resource', req.params.id);
-  next();
-});
-
-router.patch('/users/:id', (req, res, next) => {
-  validate.ownOrAnyByUserId(req.user, 'update', 'resource', req.params.id);
-  next();
-});
-
-router.put('/users/:id/role', (req, res, next) => {
-  validate.anyByUserRole(req.user, 'update', 'resource-system');
-  next();
-});
-
-module.exports = router;
+module.exports = {
+  getUsers: (req) => validate.ownOrAnyByUserId(req.user, 'read', 'resource'),
+  getUser: (req) => validate.ownOrAnyByUserId(req.user, 'read', 'resource', req.params.id),
+  updateUser: (req) => validate.ownOrAnyByUserId(req.user, 'update', 'resource', req.params.id),
+  updateUserRole: (req) => validate.anyByUserRole(req.user, 'update', 'resource-system'),
+};

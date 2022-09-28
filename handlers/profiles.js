0d4c5d911d7a2f4a8096bdfd2bdbca07c76da65c
gonenduk <gonenduk@gmail.com>
2022-09-28 13:28:27 +0300

    base and profile models

--- handlers/profiles.js
+++ handlers/profiles.js
@@ -1,13 +1,8 @@
 const BaseHandler = require('./base-handler');
-const model = require('../models/user');
+const model = require('../models/profile');
 
 const baseHandler = new BaseHandler(model);
 
 module.exports = {
-  getProfile: (req, res) => {
-    // Get profile fields only
-    req.query.projection = { username: 1 };
-
-    return baseHandler.getOne(req, res);
-  },
+  getProfile: (req, res) => baseHandler.getOne(req, res),
 };
---
+++ models/profile.js
@@ -0,0 +1,15 @@
+const BaseModel = require('./base-model');
+
+class ProfileModel extends BaseModel {
+  constructor() {
+    super('users');
+  }
+
+  getOneById(id) {
+    // Get profile fields only
+    const projection = { username: 1 };
+    return super.getOneById(id, projection);
+  }
+}
+
+module.exports = new ProfileModel();

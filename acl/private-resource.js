55903c38672d90702ebed45eb8407dbf755074c1
gonenduk <gonenduk@gmail.com>
2022-10-01 15:12:10 +0300

    make acl private resource from public one

--- acl/private-resource.js
+++ acl/private-resource.js
@@ -1,10 +1,8 @@
 const validate = require('./base-validation');
+const publicResource = require('./public-resource');
 
 module.exports = {
+  ...publicResource,
   getMany: (req) => validate.anyByUserRole(req.user, 'read', 'resource'),
-  create: (req) => validate.anyByUserRoleOrOwnByOwnerId(req.user, 'create', 'resource'),
   getOne: (req) => validate.anyByUserRoleOrOwnByOwnerId(req.user, 'read', 'resource', req.query),
-  updateOne: (req) => validate.anyByUserRoleOrOwnByOwnerId(req.user, 'update', 'resource', req.query),
-  deleteOne: (req) => validate.anyByUserRoleOrOwnByOwnerId(req.user, 'delete', 'resource', req.query),
-  updateOwner: (req) => validate.anyByUserRole(req.user, 'update', 'resource-system'),
 };
--- test/acl/private-resources.spec.js
+++ test/acl/private-resources.spec.js
@@ -4,7 +4,6 @@ const acl = require('../../acl');
 const pr = acl['private-resource'];
 
 const req = { api: true, params: {} };
-const admin = { role: 'admin', id: '1' };
 const moderator = { role: 'moderator', id: '2' };
 const user = { role: 'user', id: '3' };
 const guest = { role: 'guest' };
@@ -22,19 +21,12 @@ describe('Access control for private resources', () => {
     });
   });
 
-  context('Create resource', () => {
-    it('should allow user to create', () => {
-      req.user = user;
-      assert.doesNotThrow(() => pr.create(req));
-    });
-
-    it('should not allow guest to create', () => {
-      req.user = guest;
-      assert.throws(() => pr.create(req));
-    });
-  });
-
   context('Get resource', () => {
+    it('should not allow guest to read', () => {
+      req.user = guest;
+      assert.throws(() => pr.getMany(req));
+    });
+
     it('should limit user to read own', () => {
       req.user = user;
       req.query = {};
@@ -47,60 +39,6 @@ describe('Access control for private resources', () => {
       req.query = {};
       assert.doesNotThrow(() => pr.getOne(req));
       assert.equal(req.query.filter, undefined);
-    });
-  });
-
-  context('Update resource', () => {
-    it('should allow user to update own', () => {
-      req.user = user;
-      assert.doesNotThrow(() => pr.updateOne(req));
-    });
-
-    it('should limit moderator to update own', () => {
-      req.user = moderator;
-      req.query = {};
-      assert.doesNotThrow(() => pr.updateOne(req));
-      assert.equal(req.query.filter?.ownerId, req.user.id);
-    });
-
-    it('should allow admin to update any', () => {
-      req.user = admin;
-      assert.doesNotThrow(() => pr.updateOne(req));
-    });
-  });
-
-  context('Delete resource', () => {
-    it('should limit user to delete own', () => {
-      req.user = user;
-      req.query = {};
-      assert.doesNotThrow(() => pr.deleteOne(req));
-      assert.equal(req.query.filter?.ownerId, req.user.id);
-    });
-
-    it('should limit moderator to delete own', () => {
-      req.user = moderator;
-      req.query = {};
-      assert.doesNotThrow(() => pr.deleteOne(req));
-      assert.equal(req.query.filter?.ownerId, req.user.id);
-    });
-
-    it('should not limit admin to delete any', () => {
-      req.user = admin;
-      req.query = {};
-      assert.doesNotThrow(() => pr.deleteOne(req));
-      assert.equal(req.query.filter, undefined);
-    });
-  });
-
-  context('Update resource ownership', () => {
-    it('should not allow moderator to update own', () => {
-      req.user = moderator;
-      assert.throws(() => pr.updateOwner(req));
-    });
-
-    it('should allow admin to update any', () => {
-      req.user = admin;
-      assert.doesNotThrow(() => pr.updateOwner(req));
     });
   });
 });

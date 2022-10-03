1fa2a3799785429d63dc0ebc6851a39e8527bc9e
gonenduk <gonenduk@gmail.com>
2022-10-03 12:13:15 +0300

    products acl tests

--- test/acl/products.spec.js
+++ test/acl/products.spec.js
@@ -1,42 +1,110 @@
-const sinon = require('sinon');
-const { products } = require('../../acl');
-const PublicResource = require('../../acl/public-resource');
+const assert = require('assert').strict;
+const acl = require('../../acl');
 
-const pr = new PublicResource('ownerId');
+const { products } = acl;
+
+const req = { api: true, params: {} };
+const admin = { role: 'admin', id: '1' };
+const moderator = { role: 'moderator', id: '2' };
+const user = { role: 'user', id: '3' };
+const guest = { role: 'guest' };
 
 describe('Access control for products', () => {
-  let getMany;
-  let create;
-  let getOne;
-  let updateOne;
-  let deleteOne;
-  let updateOwner;
-
-  before(() => {
-    getMany = sinon.stub(pr, 'getMany');
-    create = sinon.stub(pr, 'create');
-    getOne = sinon.stub(pr, 'getOne');
-    updateOne = sinon.stub(pr, 'updateOne');
-    deleteOne = sinon.stub(pr, 'deleteOne');
-    updateOwner = sinon.stub(pr, 'updateOwner');
+  beforeEach(() => {
+    req.query = {};
   });
 
-  after(() => {
-    sinon.restore();
+  context('Get product list', () => {
+    it('should allow guest to read any', () => {
+      req.user = guest;
+      assert.doesNotThrow(() => products.getProducts(req));
+      assert.equal(req.query.filter, undefined);
+    });
   });
 
-  it('should be a public resource', () => {
-    products.getProducts();
-    products.createProduct();
-    products.getProduct();
-    products.updateProduct();
-    products.deleteProduct();
-    products.updateProductOwner();
-    sinon.assert.calledOnce(getMany);
-    sinon.assert.calledOnce(create);
-    sinon.assert.calledOnce(getOne);
-    sinon.assert.calledOnce(updateOne);
-    sinon.assert.calledOnce(deleteOne);
-    sinon.assert.calledOnce(updateOwner);
+  context('Create product', () => {
+    it('should not allow guest to create', () => {
+      req.user = guest;
+      assert.throws(() => products.createProduct(req));
+    });
+
+    it('should allow user to create', () => {
+      req.user = user;
+      assert.doesNotThrow(() => products.createProduct(req));
+    });
+  });
+
+  context('Get product', () => {
+    it('should allow guest to read any', () => {
+      req.user = guest;
+      assert.doesNotThrow(() => products.getProduct(req));
+      assert.equal(req.query.filter, undefined);
+    });
+  });
+
+  context('Update product', () => {
+    it('should not allow guest to update', () => {
+      req.user = guest;
+      assert.throws(() => products.updateProduct(req));
+    });
+
+    it('should limit user to update own', () => {
+      req.user = user;
+      assert.doesNotThrow(() => products.updateProduct(req));
+      assert.equal(req.query.filter, JSON.stringify({ ownerId: user.id }));
+    });
+
+    it('should limit moderator to update own', () => {
+      req.user = moderator;
+      assert.doesNotThrow(() => products.updateProduct(req));
+      assert.equal(req.query.filter, JSON.stringify({ ownerId: moderator.id }));
+    });
+
+    it('should allow admin to update any', () => {
+      req.user = admin;
+      assert.doesNotThrow(() => products.updateProduct(req));
+      assert.equal(req.query.filter, undefined);
+    });
+  });
+
+  context('Delete product', () => {
+    it('should not allow guest to delete', () => {
+      req.user = guest;
+      assert.throws(() => products.deleteProduct(req));
+    });
+
+    it('should limit user to delete own', () => {
+      req.user = user;
+      req.query = {};
+      assert.doesNotThrow(() => products.deleteProduct(req));
+      assert.equal(req.query.filter, JSON.stringify({ ownerId: user.id }));
+    });
+
+    it('should limit moderator to delete own', () => {
+      req.user = moderator;
+      req.query = {};
+      assert.doesNotThrow(() => products.deleteProduct(req));
+      assert.equal(req.query.filter, JSON.stringify({ ownerId: moderator.id }));
+    });
+
+    it('should not limit admin to delete any', () => {
+      req.user = admin;
+      req.query = {};
+      assert.doesNotThrow(() => products.deleteProduct(req));
+      assert.equal(req.query.filter, undefined);
+    });
+  });
+
+  context('Update product ownership', () => {
+    it('should not allow moderator to update', () => {
+      req.user = moderator;
+      assert.throws(() => products.updateProductOwner(req));
+    });
+
+    it('should allow admin to update any', () => {
+      req.user = admin;
+      assert.doesNotThrow(() => products.updateProductOwner(req));
+      assert.equal(req.query.filter, undefined);
+    });
   });
 });

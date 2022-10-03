a055795eb74371b51c4f8d427d9a2f4dfdcdbd0e
gonenduk <gonenduk@gmail.com>
2022-10-03 11:56:46 +0300

    profiles acl tests

--- test/acl/profiles.spec.js
+++ test/acl/profiles.spec.js
@@ -1,14 +1,17 @@
 const assert = require('assert').strict;
 const acl = require('../../acl');
 
-const req = { api: true, params: {} };
+const { profiles } = acl;
+
+const req = { api: true, params: {}, query: {} };
 const guest = { role: 'guest' };
 
 describe('Access control for profiles', () => {
   context('Get profile', () => {
     it('should allow guest to read any', () => {
       req.user = guest;
-      assert.doesNotThrow(() => acl.profiles.getProfile(req));
+      assert.doesNotThrow(() => profiles.getProfile(req));
+      assert.equal(req.query.filter, undefined);
     });
   });
 });
